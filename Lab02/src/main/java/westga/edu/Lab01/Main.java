deb3ffe7682497a38c2b54971cf98b816a46a2d3
Kyron A Joseph <kj00074@my.westga.edu>
2025-04-27 20:12:04 -0400

    Updated Lab Folder Name
    Changed the name of Lab folder changed some configurations.

---
+++ Lab02/src/main/java/westga/edu/Lab01/Main.java
@@ -0,0 +1,12 @@
+package westga.edu.Lab01;
+
+/**
+ * This class is the main method of the program
+ * @author Kyron Joseph
+ * @version 4/27/25
+ */
+public class Main {
+    public static void main(String[] args) {
+        
+    }
+}
